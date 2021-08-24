8e3f813cd93459b333806c5f0255d7d5b582f4fb
Dawid Krysiński <dawid.krysinski@iteo.com>
2021-08-24 14:53:55 +0200

    Pass rich text builder to markdown builder

--- packages/flutter_markdown/lib/src/widget.dart
+++ packages/flutter_markdown/lib/src/widget.dart
@@ -342,6 +342,7 @@ class _MarkdownWidgetState extends State<MarkdownWidget>
       listItemCrossAxisAlignment: widget.listItemCrossAxisAlignment,
       onTapText: widget.onTapText,
       softLineBreak: widget.softLineBreak,
+      richTextBuilder: widget.richTextBuilder,
     );
 
     _children = builder.build(astNodes);
